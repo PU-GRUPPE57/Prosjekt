2a282108dfc3eedd5a3de7887079739c47d93ee2
sindrefl <sindre.flood@gmail.com>
2015-02-25 16:48:10 +0100

    La til et par felt i initSQL

--- Kalender/src/users/User.java
+++ Kalender/src/users/User.java
@@ -10,6 +10,9 @@ public class User implements Listener {
 	private String id;
 	private String email;
 	private Scanner input;
+	
+	
+	
 	public void fireMessage(String message) {
 		// TODO Auto-generated method stub
 		
